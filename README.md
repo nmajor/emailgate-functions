01eb19cfa1517ba4b7baf90ce3527a4f7401797f
Nicholas Major <nick@nmajor.com>
2018-04-16 22:05:16 -0300

    initial commit with emailPdf function

---
+++ README.md
@@ -0,0 +1,43 @@
+3. **Create a Service:**
+
+  You can create a new service or [install existing services](#how-to-install-a-service).
+  ```bash
+  # Create a new Serverless Service/Project
+  serverless create --template aws-nodejs --path my-service
+  # Change into the newly created directory
+  cd my-service
+  ```
+
+4. **Deploy a Service:**
+
+  Use this when you have made changes to your Functions, Events or Resources in `serverless.yml` or you simply want to deploy all changes within your Service at the same time.
+  ```bash
+  serverless deploy -v
+  ```
+
+5. **Deploy the Function:**
+
+  Use this to quickly upload and overwrite your AWS Lambda code on AWS, allowing you to develop faster.
+  ```bash
+  serverless deploy function -f hello
+  ```
+
+6. **Invoke the Function:**
+
+  Invokes an AWS Lambda Function on AWS and returns logs.
+  ```bash
+  serverless invoke -f hello -l
+  ```
+
+7. **Fetch the Function Logs:**
+
+  Open up a separate tab in your console and stream all logs for a specific Function using this command.
+  ```bash
+  serverless logs -f hello -t
+  ```
+
+8. **Remove the Service:**
+
+  Removes all Functions, Events and Resources from your AWS account.
+  ```bash
+  serverless remove
---
+++ functions/emailPdf/emailPdf.js
@@ -0,0 +1,67 @@
+import config from '../../config';
+import * as pdfHelper from '../../lib/pdfHelper';
+import mongoConnect from '../../lib/mongoConnect';
+
+function getEmail(emailId) {
+  return new Promise((resolve, reject) => {
+    mongoConnect((db) => {
+      const collection = db.collection('emails');
+      collection.findOne({ _id: emailId }, (err, doc) => { // eslint-disable-line consistent-return
+        if (err) { return reject(err); }
+        if (!doc) { return reject(new Error('No document found.')); }
+
+        resolve(doc);
+      });
+    });
+  });
+}
+
+function buildPdf(email) {
+  console.log(`Building email ${email} pdf`);
+  const html = email.template.replace('[[BODY]]', email.body);
+  return pdfHelper.buildPdf(html, 'email', email, config.emailOptions);
+}
+
+function uploadPdf(email, pdfObj) {
+  console.log(`Uploading email ${email._id} pdf`);
+  return pdfHelper.uploadPdfObject(pdfObj, console.log);
+}
+
+function savePdfResults(email, pdfResults) {
+  return new Promise((resolve, reject) => {
+    mongoConnect((db) => {
+      const collection = db.collection('emails');
+      collection.update(
+      { _id: email._id },
+      { $set: { pdf: pdfResults } },
+      (err, result) => { // eslint-disable-line consistent-return
+        if (err) { return reject(err); }
+        if (result.result.n !== 1) { return reject(new Error('No document updated.')); }
+
+        resolve();
+      });
+    });
+  });
+}
+
+export default function emailPdf(event, context, callback) {
+  const emailId = event.pathParameters.id;
+
+  return getEmail(emailId)
+  .then(email => {
+    return buildPdf(email)
+    .then(pdfObj => uploadPdf(email, pdfObj))
+    .then(pdfResults => savePdfResults(email, pdfResults))
+    .then(() => getEmail(emailId));
+  })
+  .then(email => {
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        'Content-Type' : 'application/json',
+        'Access-Control-Allow-Origin' : '*',
+      },
+      body: JSON.stringify(email),
+    });
+  })
+}
